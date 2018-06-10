978b3e08d3b6119071e5173792080befd449d6fa
Clemens <Clemens@Clemens-PC>
2018-06-10 21:48:53 +0200

    HÜ-Versuch

--- MyFirstGUI/src/data/Person.java
+++ MyFirstGUI/src/data/Person.java
@@ -7,12 +7,7 @@ import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
 import java.lang.reflect.Type;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
